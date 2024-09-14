eb74adba791c745386818935136750e32b8bc86e
mariliisp <mariliis.pihlapuu@javan.ee>
2024-09-14 15:43:30 +0300

    Include externally generated Spring boot application base

---
+++ src/main/java/smit/homework/bookloan/BookloanApplication.java
@@ -0,0 +1,13 @@
+package smit.homework.bookloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smit/homework/bookloan/BookloanApplicationTests.java
@@ -0,0 +1,13 @@
+package smit.homework.bookloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookloanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
