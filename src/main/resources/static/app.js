194c9904a0f6a067b76a986d5b27aa0eb228004d
mariliisp <mariliis.pihlapuu@javan.ee>
2024-09-14 19:27:09 +0300

    Working WIP state of AngularJS app

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,12 +1,53 @@
-var app = angular.module('myApp', []);
+var app = angular.module("BookLoanManagement", []);
 
-app.controller('BookLoanController', function($scope, $http) {
-    $http({
-        method : 'GET',
-        url : 'http://localhost:8080/api/greet'
-    }).then(function successCallback(response) {
-        $scope.message = response.data;
-    }, function errorCallback(response) {
+app.controller("BookLoanController", function ($scope, $http) {
+    $scope.books = [];
+    $scope.bookForm = {
+        id: -1,
+        title: "",
+        author: ""
+    };
+
+    _refreshBookData();
+
+    $scope.saveNewBook = function () {
+        $http({
+            method: 'POST',
+            url: 'api/books',
+            data: angular.toJSON($scope.bookForm),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(_success, _error);
+    };
+
+    /* Private Methods */
+
+    //HTTP GET- get all customers collection
+    function _refreshBookData() {
+        $http({
+            method: 'GET',
+            url: 'http://localhost:8080/api/books'
+        }).then(function successCallback(response) {
+            $scope.books = response.data;
+        }, function errorCallback(response) {
+            console.log(response.statusText);
+        });
+    }
+
+    function _success(response) {
+        _refreshBookData();
+        _clearFormData()
+    }
+
+    function _error(response) {
         console.log(response.statusText);
-    });
-});
+    }
+
+    //Clear the form
+    function _clearFormData() {
+        $scope.bookForm.id = -1;
+        $scope.bookForm.customerName = "";
+        $scope.bookForm.email = "";
+    };
+})
